ff64cc13deaabe2b224eaa6a547335a291873f49
Jack Sparrow <capjacksparrow42@protonmail.com>
2022-11-03 17:37:58 +0530

    mangata x base apr

--- src/tasks/mangata.ts
+++ src/tasks/mangata.ts
@@ -1,4 +1,4 @@
-import { Mangata } from '@mangata-finance/sdk';
+import { Mangata, TMainTokens } from '@mangata-finance/sdk';
 import { BN } from 'bn.js';
 
 import Farm from "../models/farm";
@@ -6,6 +6,176 @@ import Farm from "../models/farm";
 import { collections } from '../services/database.service';
 
 import axios from 'axios';
+
+import util from "util";
+import { exec } from "child_process";
+
+import fs from "fs"
+
+const exec1 = util.promisify(exec);
+
+// events
+export interface EventsRoot {
+    code: number
+    message: string
+    generated_at: number
+    data: EventsData
+}
+
+export interface EventsData {
+    count: number
+    events: Event[]
+}
+
+export interface Event {
+    id: number
+    block_timestamp: number
+    event_index: string
+    extrinsic_index: string
+    phase: number
+    module_id: string
+    event_id: string
+    extrinsic_hash: string
+    finalized: boolean
+}
+
+// extrinsic
+export interface ExtrinsicRoot {
+    code: number
+    message: string
+    generated_at: number
+    data: ExtrinsicData
+}
+
+export interface ExtrinsicData {
+    block_timestamp: number
+    block_num: number
+    extrinsic_index: string
+    call_module_function: string
+    call_module: string
+    account_id: string
+    signature: string
+    nonce: number
+    extrinsic_hash: string
+    success: boolean
+    params: Param[]
+    transfer: any
+    event: ExtrinsicEvent[]
+    event_count: number
+    fee: string
+    fee_used: string
+    error: any
+    finalized: boolean
+    lifetime: Lifetime
+    tip: string
+    account_display: AccountDisplay
+    crosschain_op: any
+    block_hash: string
+    pending: boolean
+}
+
+export interface Param {
+    name: string
+    type: string
+    type_name: string
+    value: any
+}
+
+export interface ExtrinsicEvent {
+    event_index: string
+    block_num: number
+    extrinsic_idx: number
+    module_id: string
+    event_id: string
+    params: string
+    phase: number
+    event_idx: number
+    extrinsic_hash: string
+    finalized: boolean
+    block_timestamp: number
+}
+
+export interface Lifetime {
+    birth: number
+    death: number
+}
+
+export interface AccountDisplay {
+    address: string
+}
+
+// block
+export interface BlockRoot {
+    code: number
+    message: string
+    generated_at: number
+    data: BlockData
+}
+
+export interface BlockData {
+    block_num: number
+    block_timestamp: number
+    // hash: string
+    // parent_hash: string
+    // state_root: string
+    // extrinsics_root: string
+    // extrinsics: Extrinsic[]
+    // events: BlockEvent[]
+    // logs: Log[]
+    // event_count: number
+    // extrinsics_count: number
+    // spec_version: number
+    // validator: string
+    // finalized: boolean
+    // account_display: AccountDisplay
+}
+
+export interface Extrinsic {
+    block_timestamp: number
+    block_num: number
+    extrinsic_index: string
+    call_module_function: string
+    call_module: string
+    params: string
+    account_id: string
+    account_index: string
+    signature: string
+    nonce: number
+    extrinsic_hash: string
+    success: boolean
+    fee: string
+    fee_used: string
+    from_hex: string
+    finalized: boolean
+    account_display: any
+}
+
+export interface BlockEvent {
+    event_index: string
+    block_num: number
+    extrinsic_idx: number
+    module_id: string
+    event_id: string
+    params: string
+    phase: number
+    event_idx: number
+    extrinsic_hash: string
+    finalized: boolean
+    block_timestamp: number
+}
+
+export interface Log {
+    id: number
+    block_num: number
+    log_index: string
+    log_type: string
+    engine: string
+    data: string
+}
+
+//   export interface AccountDisplay {
+//     address: string
+//   }
 
 
 export const runMangataTask = async () => {
@@ -20,9 +190,9 @@ export const runMangataTask = async () => {
 
     let rwd_pools_count = 2
 
-    let mgxDecimals: number = assetsInfo['0']['decimals']
-    let ksmDecimals: number = assetsInfo['4']['decimals']
-    let turDecimals: number = assetsInfo['7']['decimals']
+    let mgxDecimals: number = assetsInfo[0]['decimals']
+    let ksmDecimals: number = assetsInfo[4]['decimals']
+    let turDecimals: number = assetsInfo[7]['decimals']
 
     console.log("mgxDecimals", mgxDecimals, "turDecimals", turDecimals, "ksmDecimals", ksmDecimals);
 
@@ -42,10 +212,7 @@ export const runMangataTask = async () => {
     let c0 = balance07.toString().split(",")[0]
     let c1 = balance07.toString().split(",")[1]
 
-
     console.log("balance40[0]", balance40[0], "balance40[1]", balance40[1], "b0", new BN(b0), "b1", new BN(b1));
-
-
 
     const amountPool40 = await mangata.getAmountOfTokenIdInPool("4", "0");
     const ksmReserve40 = amountPool40[0];
@@ -67,12 +234,8 @@ export const runMangataTask = async () => {
         new BN((10 ** turDecimals).toString()) // 1tur = 1_000_000_000_0
     );
 
-
     console.log("mgxBuyPriceInKsm", mgxBuyPriceInKsm, "turBuyPriceInMgx", turBuyPriceInMgx);
 
-    // // fromBN method is used to display nice value
-    // // 12 here means decimals for KSM
-    // const mgxToKsm = fromBN(mgxBuyPriceInKsm, 12); // 1 mgx = ?? ksm
     const mgxInKsm = mgxBuyPriceInKsm.toNumber() / 10 ** ksmDecimals;
 
     const turInMgx = turBuyPriceInMgx.div(new BN((10 ** mgxDecimals).toString())).toNumber();
@@ -84,24 +247,264 @@ export const runMangataTask = async () => {
     const ksmInUsd = cgkres?.data?.kusama?.usd ?? 35.66;
     console.log("ksmInUsd", ksmInUsd);
 
-    // console.log("ksmres", parseInt(b0), parseInt(b0) / 10 ** ksmDecimals, "mgxres", parseInt(b1), parseInt(b1) / 10 ** mgxDecimals);
-
     console.log("ksm-mgx tvl: $", ksmInUsd * (parseInt(b0) / 10 ** ksmDecimals + (mgxInKsm * parseInt(b1) / 10 ** mgxDecimals)));
     console.log("mgx-tur tvl: $", ksmInUsd * (mgxInKsm * parseInt(c0) / 10 ** mgxDecimals + (turInKsm * parseInt(c1) / 10 ** turDecimals)));
 
     const rewards_per_day = ksmInUsd * mgxInKsm * (300 * 10 ** 6 / (2 * 365))
     console.log("rewards_per_day: $", rewards_per_day, "or ", (300 * 10 ** 6 / (2 * 365)), "mgx");
 
+    // base_apr
+
+    let baseAPRKsmMgx = 0;
+    let baseAPRMgxTur = 0;
+
+    const getDecimals = (assetId: number, assetsInfo: TMainTokens) => {
+        console.log("assetsInfo", assetsInfo[assetId.toString()].decimals, "assetId", assetId);
+
+        return assetsInfo[assetId.toString()].decimals
+    }
+
+    // subtract 10 seconds from time to account for possible delay in subscan api
+    const timeNow = Math.floor(Date.now() / 1000) - 10
+    const timeNowLastWeek = Math.floor(Date.now() / 1000) - 10 - (7 * 24 * 60 * 60)
+
+    let blockNow = -1
+    let blockNowLastWeek = -1
+
+    const res = await axios({
+        method: 'post',
+        url: 'https://mangatax.webapi.subscan.io/api/scan/block',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': `${process.env.SUBSCAN_API_KEY}`,
+            'User-Agent': '',
+        },
+        data: JSON.stringify({
+            "block_timestamp": timeNow
+        })
+    })
+
+    let d: BlockRoot;
+
+    d = JSON.parse(JSON.stringify(res.data))
+
+    console.log("blocknum", d?.data?.block_num ?? -1);
+    blockNow = d?.data?.block_num ?? -1;
+
+    const res1 = await axios({
+        method: 'post',
+        url: 'https://mangatax.webapi.subscan.io/api/scan/block',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': `${process.env.SUBSCAN_API_KEY}`,
+            'User-Agent': '',
+        },
+        data: JSON.stringify({
+            "block_timestamp": timeNowLastWeek
+        })
+    })
+
+    let d1: BlockRoot;
+
+    d1 = JSON.parse(JSON.stringify(res1.data))
+
+    console.log("blocknum", d1?.data?.block_num ?? -1);
+    blockNowLastWeek = d1?.data?.block_num ?? -1;
+
+    console.log("blockNow", blockNow, "blockNowLastWeek", blockNowLastWeek);
+
+    if (blockNowLastWeek !== -1 && blockNow !== -1) {
+        let events: any = [];
+        const res2 = await axios({
+            method: 'post',
+            url: 'https://mangatax.webapi.subscan.io/api/v2/scan/events',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-API-Key': `${process.env.SUBSCAN_API_KEY}`,
+                'User-Agent': '',
+            },
+            data: JSON.stringify({
+                "address": "", "row": 100, "page": 0, "module": "xyk", "event_id": "assetsswapped", "block_range": `${blockNowLastWeek}-${blockNow}`
+            })
+        })
+
+        let d2: EventsRoot;
+
+        d2 = JSON.parse(JSON.stringify(res2.data))
+
+        let dc = d2?.data?.count;
+        console.log("dc", dc);
+
+        events = events.concat(d2?.data?.events)
+
+        if (dc > 100) {
+            let i = Math.floor(dc / 100)
+            for (let idx = 1; idx <= i; idx++) {
+                const resi = await axios({
+                    method: 'post',
+                    url: 'https://mangatax.webapi.subscan.io/api/v2/scan/events',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-API-Key': `${process.env.SUBSCAN_API_KEY}`,
+                        'User-Agent': '',
+                    },
+                    data: JSON.stringify({
+                        "address": "", "row": 100, "page": idx, "module": "xyk", "event_id": "assetsswapped", "block_range": `${blockNowLastWeek}-${blockNow}`
+                    })
+                })
+
+                let di: EventsRoot;
+
+                di = JSON.parse(JSON.stringify(resi.data))
+
+                let dci = di?.data?.count;
+                console.log("dci", dci);
+
+                events = events.concat(di?.data?.events)
+            }
+        }
+
+        let swaps: any[] = []
+
+        console.log("final events len", events.length);
+
+        let myarr = await Promise.all(events.map(async (ev: Event) => {
+            console.log("ev.extrinsic_index", ev.extrinsic_index)
+
+            const res = await axios({
+                method: 'post',
+                url: 'https://mangatax.webapi.subscan.io/api/scan/extrinsic',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-API-Key': `${process.env.SUBSCAN_API_KEY}`,
+                    'User-Agent': '',
+                },
+                data: JSON.stringify({
+                    "extrinsic_index": `${ev.extrinsic_index}`,
+                    "events_limit": 10,
+                    "focus": `${ev.extrinsic_index}`
+                })
+            })
+
+            let d3: ExtrinsicRoot;
+
+            d3 = JSON.parse(JSON.stringify(res.data))
+            console.log("d1i", d3.data.extrinsic_index);
+
+            let soldAsset = -1;
+            let soldAmount = "0";
+            let soldAmountUsd = 0;
+            let boughtAsset = -1;
+            let boughtAmount = "0";
+            let boughtAmountUsd = 0;
+
+            d3.data.params.forEach((p: Param) => {
+                console.log("param", p);
+                if (p.name == "sold_asset_id") {
+                    soldAsset = p.value
+                } else if (p.name == "sold_asset_amount") {
+                    console.log("saa", p.value);
+                    soldAmount = p.value;
+                } else if (p.name == "bought_asset_id") {
+                    boughtAsset = p.value
+                } else if (p.name == "bought_asset_amount") {
+                    console.log("baa", p.value);
+                    boughtAmount = p.value;
+                }
+            })
+
+            let swapType = "";
+
+            if (soldAsset !== -1) {
+                let sa = (parseInt(soldAmount, 10) / 10 ** getDecimals(soldAsset, assetsInfo), 10).toString()
+                if (soldAsset == 4) {
+                    // ksm
+                    soldAmountUsd = parseInt(sa, 10) * ksmInUsd
+                } else if (soldAsset == 0) {
+                    // mgx
+                    soldAmountUsd = parseInt(sa, 10) * mgxInKsm * ksmInUsd
+                } else if (soldAsset == 7) {
+                    // tur
+                    soldAmountUsd = parseInt(sa, 10) * turInKsm * ksmInUsd
+                }
+                return {
+                    soldAsset: soldAsset,
+                    boughtAsset: boughtAsset,
+                    amount: soldAmount,
+                    amountUsd: soldAmountUsd,
+                    swapType: "sell"
+                }
+            } else if (boughtAsset !== -1) {
+                let ba = (parseInt(boughtAmount, 10) / 10 ** getDecimals(boughtAsset, assetsInfo), 10).toString()
+                if (boughtAsset == 4) {
+                    // ksm
+                    boughtAmountUsd = parseInt(ba, 10) * ksmInUsd
+                } else if (boughtAsset == 0) {
+                    // mgx
+                    boughtAmountUsd = parseInt(ba, 10) * mgxInKsm * ksmInUsd
+                } else if (boughtAsset == 7) {
+                    // tur
+                    boughtAmountUsd = parseInt(ba, 10) * turInKsm * ksmInUsd
+                }
+                return {
+                    soldAsset: soldAsset,
+                    boughtAsset: boughtAsset,
+                    amount: boughtAmount,
+                    amountUsd: boughtAmountUsd,
+                    swapType: "buy"
+                }
+            }
+        }))
+
+        if (myarr == undefined || typeof myarr == undefined) {
+            swaps = []
+        } else {
+            swaps = myarr
+        }
+        console.log("swapslen", swaps.length, swaps);
+
+        let dailyVolumeLWKsmMgx = 0
+        let dailyVolumeLWMgxTur = 0
+
+        swaps.forEach((swap) => {
+            if (swap !== undefined && typeof swap !== undefined) {
+                if ((swap.boughtAsset == 4 && swap.soldAsset == 0) || (swap.boughtAsset == 0 && swap.soldAsset == 4)) {
+                    // ksm-mgx
+                    dailyVolumeLWKsmMgx += swap.amountUsd
+                }
+                if ((swap.boughtAsset == 0 && swap.soldAsset == 7) || (swap.boughtAsset == 7 && swap.soldAsset == 0)) {
+                    // mgx-tur
+                    dailyVolumeLWMgxTur += swap.amountUsd
+                }
+            }
+        })
+
+        console.log("dailyVolumeLWKsmMgx", dailyVolumeLWKsmMgx);
+        console.log("dailyVolumeLWMgxTur", dailyVolumeLWMgxTur);
+
+        dailyVolumeLWKsmMgx /= 7
+        dailyVolumeLWMgxTur /= 7
+
+        console.log("dailyVolumeLWKsmMgx", dailyVolumeLWKsmMgx);
+        console.log("dailyVolumeLWMgxTur", dailyVolumeLWMgxTur);
+
+        baseAPRKsmMgx = (dailyVolumeLWKsmMgx * 0.002 * 365 * 100) / (ksmInUsd * (parseInt(b0) / 10 ** ksmDecimals + (mgxInKsm * parseInt(b1) / 10 ** mgxDecimals)))
+        baseAPRMgxTur = (dailyVolumeLWMgxTur * 0.002 * 365 * 100) / (ksmInUsd * (mgxInKsm * parseInt(c0) / 10 ** mgxDecimals + (turInKsm * parseInt(c1) / 10 ** turDecimals)))
+
+        console.log("baseAPRKsmMgx", baseAPRKsmMgx);
+        console.log("baseAPRMgxTur", baseAPRMgxTur);
+    }
+
     collections.farms?.findOneAndUpdate({
         "id": 5,
         "chef": "xyk",
-        "chain": "Kusama",
+        "chain": "Mangata Kusama",
         "protocol": "Mangata X",
     }, {
         "$set": {
             "id": 5,
             "chef": "xyk",
-            "chain": "Kusama",
+            "chain": "Mangata Kusama",
             "protocol": "Mangata X",
             "farmType": "StandardAmm",
             "farmImpl": "Pallet",
@@ -115,8 +518,8 @@ export const runMangataTask = async () => {
                 ],
             },
             "tvl": ksmInUsd * (parseInt(b0) / 10 ** ksmDecimals + (mgxInKsm * parseInt(b1) / 10 ** mgxDecimals)),
-            "apr.reward": rewards_per_day,
-            "apr.base": 0.0,
+            "apr.reward": ksm_mgx_apr,
+            "apr.base": baseAPRKsmMgx,
             "rewards": [
                 {
                     "amount": (300 * 10 ** 6 / (2 * 365)),
@@ -140,13 +543,13 @@ export const runMangataTask = async () => {
     collections.farms?.findOneAndUpdate({
         "id": 8,
         "chef": "xyk",
-        "chain": "Kusama",
+        "chain": "Mangata Kusama",
         "protocol": "Mangata X",
     }, {
         "$set": {
             "id": 8,
             "chef": "xyk",
-            "chain": "Kusama",
+            "chain": "Mangata Kusama",
             "protocol": "Mangata X",
             "farmType": "StandardAmm",
             "farmImpl": "Pallet",
@@ -160,8 +563,8 @@ export const runMangataTask = async () => {
                 ],
             },
             "tvl": ksmInUsd * (mgxInKsm * parseInt(c0) / 10 ** mgxDecimals + (turInKsm * parseInt(c1) / 10 ** turDecimals)),
-            "apr.reward": rewards_per_day,
-            "apr.base": 0.0,
+            "apr.reward": mgx_tur_apr,
+            "apr.base": baseAPRMgxTur,
             "rewards": [
                 {
                     "amount": (300 * 10 ** 6 / (2 * 365)),
