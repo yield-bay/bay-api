4052179c63cc5df61a7c65ac777b2a60b2b7be82
Jack Sparrow <capjacksparrow42@protonmail.com>
2023-02-18 15:37:48 +0530

    add filter for chain and protocol

--- src/resolvers/farms.resolver.ts
+++ src/resolvers/farms.resolver.ts
@@ -12,23 +12,101 @@ export const FarmsResolver = async (parents: any, args: any, context: any) => {
             .toArray()) as unknown as Farm[]
         console.log("farms count", dbFarms.length)
 
-        const farms = dbFarms.map((f: Farm) => {
-            return {
-                id: f.id,
-                chef: f.chef,
-                chain: f.chain,
-                protocol: f.protocol,
-                farmType: f.farmType,
-                farmImpl: f.farmImpl,
-                asset: f.asset,
-                tvl: f.tvl,
-                apr: f.apr,
-                rewards: f.rewards,
-                allocPoint: f.allocPoint,
-                lastUpdatedAtUTC: f.lastUpdatedAtUTC,
-                safetyScore: f.totalScore,
-            }
-        })
+        const isChain = (f: Farm, chain: string) => {
+            return f.chain == chain;
+        }
+
+        const isProtocol = (f: Farm, protocol: string) => {
+            return f.protocol == protocol;
+        }
+
+        let farms: any[] = [];
+
+        if (args?.chain !== undefined && args?.protocol !== undefined) {
+            console.log("c1", args?.chain);
+            farms = dbFarms.filter((f: Farm) => {
+                if (isChain(f, args?.chain) && isProtocol(f, args?.protocol)) {
+                    console.log("valid");
+                    return {
+                        id: f.id,
+                        chef: f.chef,
+                        chain: f.chain,
+                        protocol: f.protocol,
+                        farmType: f.farmType,
+                        farmImpl: f.farmImpl,
+                        asset: f.asset,
+                        tvl: f.tvl,
+                        apr: f.apr,
+                        rewards: f.rewards,
+                        allocPoint: f.allocPoint,
+                        lastUpdatedAtUTC: f.lastUpdatedAtUTC,
+                        safetyScore: f.totalScore,
+                    }
+                }
+            })
+        } else if (args?.chain !== undefined && args?.protocol == undefined) {
+            console.log("c2", args?.chain);
+            farms = dbFarms.filter((f: Farm) => {
+                if (isChain(f, args?.chain))
+                    return {
+                        id: f.id,
+                        chef: f.chef,
+                        chain: f.chain,
+                        protocol: f.protocol,
+                        farmType: f.farmType,
+                        farmImpl: f.farmImpl,
+                        asset: f.asset,
+                        tvl: f.tvl,
+                        apr: f.apr,
+                        rewards: f.rewards,
+                        allocPoint: f.allocPoint,
+                        lastUpdatedAtUTC: f.lastUpdatedAtUTC,
+                        safetyScore: f.totalScore,
+                    }
+            })
+        } else if (args?.chain == undefined && args?.protocol !== undefined) {
+            console.log("c3", args?.chain);
+            farms = dbFarms.filter((f: Farm) => {
+                if (isProtocol(f, args?.protocol))
+                    return {
+                        id: f.id,
+                        chef: f.chef,
+                        chain: f.chain,
+                        protocol: f.protocol,
+                        farmType: f.farmType,
+                        farmImpl: f.farmImpl,
+                        asset: f.asset,
+                        tvl: f.tvl,
+                        apr: f.apr,
+                        rewards: f.rewards,
+                        allocPoint: f.allocPoint,
+                        lastUpdatedAtUTC: f.lastUpdatedAtUTC,
+                        safetyScore: f.totalScore,
+                    }
+            })
+        } else {
+            console.log("c1undef");
+            farms = dbFarms.map((f: Farm) => {
+                return {
+                    id: f.id,
+                    chef: f.chef,
+                    chain: f.chain,
+                    protocol: f.protocol,
+                    farmType: f.farmType,
+                    farmImpl: f.farmImpl,
+                    asset: f.asset,
+                    tvl: f.tvl,
+                    apr: f.apr,
+                    rewards: f.rewards,
+                    allocPoint: f.allocPoint,
+                    lastUpdatedAtUTC: f.lastUpdatedAtUTC,
+                    safetyScore: f.totalScore,
+                }
+            })
+        }
+
+        console.log("flen", farms.length);
+
         return farms
     } catch (error: any) {
         console.log(error.message)
